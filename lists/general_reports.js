4d3541113e6dbbd8fe9f607782154ded36111815
whimboo <hskupin@mozilla.com>
2010-09-12 00:01:45 -0700

    Add branch to the key for reports and add pagination

--- lists/general_reports.js
+++ lists/general_reports.js
@@ -1,32 +1,54 @@
 function(head, req) {
-  var ddoc = this;
   var mustache = require("vendor/couchapp/lib/mustache");
 
+  var ddoc = this;
   data = {
     title : "Firefox General Testrun Reports",
     site_title : ddoc.couchapp.name,
+    path : "/general/reports",
     reports : []
   };
 
   provides("html", function() {
-    while (row = getRow()) {
-      var value = row.value;
+    var row = getRow();
+    var key, value;
+    var index = 1;
 
-      data.reports.push({
-        id : value.id,
-        time_start : value.time_start,
-        time_end : value.time_end,
-        version : value.application_version,
-        buildid : value.buildId,
-        locale : value.application_locale,
-        system : value.system,
-        system_version : value.system_version,
-        cpu : value.processor,
-        passed : value.tests_passed,
-        failed : value.tests_failed,
-        skipped : value.tests_skipped
-      });
+    if (row) {
+      if (head.offset > 0)
+        data.key_prev = row.key[0];
+  
+      do {
+        key = row.key;
+        value = row.value;
+
+        if (index < req.query.limit) {
+          data.reports.push({
+            id : row.id,
+            index : head.offset + index,
+            time_start : value.time_start,
+            time_end : value.time_end,
+            application_version : value.application_version,
+            application_branch : row.key[1],
+            platform_branch : value.platform_branch,
+            buildId : value.buildId,
+            locale : value.application_locale,
+            system : value.system,
+            system_version : value.system_version,
+            processor : value.processor,
+            tests_passed : value.tests_passed,
+            tests_failed : value.tests_failed,
+            tests_skipped : value.tests_skipped
+          });
+        }
+  
+        index++;
+      } while (row = getRow());
+
+      if (head.offset + index < head.total_rows)
+        data.key_next = key[0];
     }
+
     send(mustache.to_html(ddoc.templates.general.reports, data, ddoc.templates.partials));
   });
 };
--- views/general_reportsByDate/map.js
+++ views/general_reportsByDate/map.js
@@ -1,16 +1,25 @@
 function(doc) {
   var report_types = ['firefox-general', 'mozmill', 'mozmill-restart'];
+  var platform_versions = {'4.0' : 'mozilla2.0',
+                           '3.6' : 'mozilla1.9.2',
+                           '3.5' : 'mozilla1.9.1'
+                          };
   
   if (doc.time_start &&
       doc.report_type && report_types.indexOf(doc.report_type) != -1) {
 
-    emit(doc.time_start, {
+      var application_branch = doc.application_version.match(/(\d\.\d)\.*/)[1];
+      var platform_branch = platform_versions[application_branch];
+
+    emit([doc.time_start, application_branch],
+    {
       id: doc._id,
       time_start : doc.time_start,
       time_end : doc.time_end,
       application_name : doc.application_name,
       application_version : doc.application_version,
       application_locale : doc.application_locale,
+      platform_branch : platform_branch,
       buildId : doc.platform_buildid,
       system : doc.system_info.system,
       system_version : doc.system_info.version,
