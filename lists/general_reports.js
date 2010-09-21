5023da627cd4a86aaf14951402fa6f731ea463ce
whimboo <hskupin@mozilla.com>
2010-09-22 01:32:28 +0200

    Updated templates, css, and added filters

--- lists/general_reports.js
+++ lists/general_reports.js
@@ -2,51 +2,44 @@ function(head, req) {
   var mustache = require("vendor/couchapp/lib/mustache");
 
   var ddoc = this;
+
   data = {
-    title : "Firefox General Testrun Reports",
     site_title : ddoc.couchapp.name,
+    page_title : "Firefox General Testrun Reports",
     path : "/general/reports",
     reports : []
   };
 
   provides("html", function() {
-    var row = getRow();
-    var key, value;
+    var key, report;
     var index = 1;
 
+    var row = getRow();
+
     if (row) {
-      if (head.offset > 0)
+      if (head.offset > 0) {
         data.key_prev = row.key[0];
+        data.docid_prev = row.id;
+      }
   
       do {
         key = row.key;
-        value = row.value;
+        report = row.value;
 
         if (index < req.query.limit) {
-          data.reports.push({
-            id : row.id,
-            index : head.offset + index,
-            time_start : value.time_start,
-            time_end : value.time_end,
-            application_version : value.application_version,
-            application_branch : row.key[1],
-            platform_branch : value.platform_branch,
-            buildId : value.buildId,
-            locale : value.application_locale,
-            system : value.system,
-            system_version : value.system_version,
-            processor : value.processor,
-            tests_passed : value.tests_passed,
-            tests_failed : value.tests_failed,
-            tests_skipped : value.tests_skipped
-          });
+          report.id = row.id;
+          report.index = head.offset + index,
+          report.application_branch = key[1];
+          data.reports.push(report);
         }
-  
+
         index++;
       } while (row = getRow());
 
-      if (head.offset + index < head.total_rows)
+      if (head.offset + index < head.total_rows) {
         data.key_next = key[0];
+        data.docid_next = report.id;
+      }
     }
 
     send(mustache.to_html(ddoc.templates.general.reports, data, ddoc.templates.partials));
