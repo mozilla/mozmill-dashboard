9c0b22e0884f948bcf2976d0c87abe76dfe755fd
Henrik Skupin <mail@hskupin.info>
2012-04-28 10:12:16 +0200

    Update version filter for Firefox 15.0 and drop of Firefox 11.0

--- _attachments/js/config.js
+++ _attachments/js/config.js
@@ -1,4 +1,4 @@
-var FIREFOX_VERSIONS = ["14.0", "13.0", "12.0", "11.0", "10.0", "3.6"];
+var FIREFOX_VERSIONS = ["15.0", "14.0", "13.0", "12.0", "10.0", "3.6"];
 
 var BYTE_TO_MEGABYTE = 1/1048576;
 var MAX_CHART_CHECKPOINTS = 450;
