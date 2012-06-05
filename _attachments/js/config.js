43b34a3b9fd461995de50c8b07d82c4dc059213a
Henrik Skupin <mail@hskupin.info>
2012-06-05 13:21:27 +0200

    Add version 16.0 and remove obsolete ones. r=dburns

--- _attachments/js/config.js
+++ _attachments/js/config.js
@@ -1,4 +1,4 @@
-var FIREFOX_VERSIONS = ["15.0", "14.0", "13.0", "12.0", "10.0", "3.6"];
+var FIREFOX_VERSIONS = ["16.0", "15.0", "14.0", "13.0", "10.0"];
 var TESTS_REPOSITORY = "http://hg.mozilla.org/qa/mozmill-tests";
 
 var BYTE_TO_MEGABYTE = 1/1048576;
