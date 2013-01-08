32347f1c7d0e21a3d0476532cb1249fe38d73c05
Henrik Skupin <mail@hskupin.info>
2013-01-08 15:08:03 +0100

    Bump max Firefox version to 21.0

--- _attachments/js/config.js
+++ _attachments/js/config.js
@@ -1,4 +1,4 @@
-var FIREFOX_VERSIONS = ["20.0", "19.0", "18.0", "17.0", "10.0"];
+var FIREFOX_VERSIONS = ["21.0", "20.0", "19.0", "18.0", "17.0", "10.0"];
 var TESTS_REPOSITORY = "http://hg.mozilla.org/qa/mozmill-tests";
 
 var BYTE_TO_MEGABYTE = 1/1048576;
